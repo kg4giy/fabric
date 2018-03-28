a45c728d0ac5bfd1f2125300abb69f203b378858
David A. Lane <kg4giy@gmail.com>
2018-03-28 15:01:38 -0400

    Created the file moving script

---
+++ files/fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import env
+from fabric.operations import run, put
+
+env.hosts = [
+    'pi@192.168.20.101',
+    'pi@192.168.20.103',
+    'pi@192.168.20.106'
+]
+
+env.password = 'password'
+
+def copy():
+	# Make sure the directory is there
+	run('mkdir -p /home/pi/.ssh')
+
+	#our local directory to copy
+	put('/home/pi/.ssh', '/home/pi/.ssh')
