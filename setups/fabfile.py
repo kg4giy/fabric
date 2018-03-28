c8665d27c214f58dc7db068b795d0db284a51aae
David A. Lane <kg4giy@gmail.com>
2018-03-28 15:26:09 -0400

    Updated setups for a consistant single file. Probably not the right way to do it.

--- setups/fabfile.py
+++ setups/fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+from fabric.operations import run, put
 
 env.hosts = [
     'pi@192.168.20.101',
@@ -6,8 +7,24 @@ env.hosts = [
     'pi@192.168.20.106'
 ]
 
-env.password = 'password'
+# Alternate for AWS
+# env.hosts=["user@instance-region.compute.amazonaws.com"]
+
+env.key_filename = '/home/pi/.ssh/pi.pem'
+# Alternate
+# env.key_filename=['/home/pi/.ssh/pi.pem']
+# env.password = 'password'
+
+# Reference https://stackoverflow.com/questions/5327465/using-an-ssh-keyfile-with-fabric/28409635
 
 @parallel
 def cmd(command):
     sudo(command)
+
+
+def copy():
+	# Make sure the directory is there
+	run('mkdir -p /home/pi/.ssh')
+
+	#our local directory to copy
+	put('/home/pi/.ssh/authorized_keys', '/home/pi/.ssh')
