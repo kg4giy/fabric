d2dd7f482d9b1c769a29c663a4db3880882e18f8
David A. Lane <kg4giy@gmail.com>
2019-03-07 17:45:22 -0500

    Guage python

--- web/HelloWorld.py
+++ web/HelloWorld.py
@@ -1,22 +1,21 @@
 import http.server
-import random
+import time
 from prometheus_client import start_http_server
-from prometheus_client import Counter
+from prometheus_client import Gauge
 
-REQUESTS = Counter('hello_worlds_total',
-        'Hello Worlds requested.')
-EXCEPTIONS = Counter('hello_world_exceptions_total',
-        'Exceptions serving Hello World.')
+INPROGRESS = Gauge('hello_worlds_inprogress',
+        'Number of Hello Worlds in progress.')
+LAST = Gauge('hello_world_last_time_seconds',
+        'The last time a Hello World was served.')
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        REQUESTS.inc()
-        with EXCEPTIONS.count_exceptions():
-          if random.random() < 0.2:
-            raise Exception
+        INPROGRESS.inc()
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
+        LAST.set(time.time())
+        INPROGRESS.dec()
 
 if __name__ == "__main__":
     start_http_server(8000)
