d079c78581b0494c903143fd96b125ada2ea6084
David A. Lane <kg4giy@gmail.com>
2019-03-07 17:18:38 -0500

    Updated HelloWorld.py for counting.

--- web/HelloWorld.py
+++ web/HelloWorld.py
@@ -1,5 +1,5 @@
 import http.server
-from prometheus_client import Counter
+from prometheus_client import start_http_server Counter
 
 REQUESTS = Counter('hello_worlds_total',
         'Hello Worlds requested.')
@@ -10,3 +10,8 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello World")
+
+if __name__ == "__main__":
+    start_http_server(8000)
+    server = http.server.HTTPServer(('localhost', 8001), MyHandler)
+    server.serve_forever()
