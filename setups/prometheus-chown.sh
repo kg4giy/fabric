426d19216df26505cc49b55f5da70f752721d273
David A. Lane <kg4giy@gmail.com>
2019-03-08 11:42:34 -0500

    Updated for sudorun

--- setups/prometheus-chown.sh
+++ setups/prometheus-chown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Set the permissions
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
-sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+chown prometheus:prometheus /usr/local/bin/prometheus
+chown prometheus:prometheus /usr/local/bin/promtool
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
