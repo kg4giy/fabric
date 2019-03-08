1137645ffe52f071b310b5802fa085fa9e93f366
David A. Lane <kg4giy@gmail.com>
2019-03-08 11:42:04 -0500

    Testing file.

---
+++ setups/prometheus-chown.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Set the permissions
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
